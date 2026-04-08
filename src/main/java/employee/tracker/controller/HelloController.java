ffc6cf553fde3e71209aadb27b870fcd4ad698dc
Bayanda Mnguni <bayandaPM@gmail.com>
2026-04-08 12:53:43 +0200

    Initial Spring Boot setup

---
+++ src/main/java/employee/tracker/controller/HelloController.java
@@ -0,0 +1,15 @@
+package employee.tracker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloController {
+    
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello from Employee Tracker!";
+    }
+    
+}
