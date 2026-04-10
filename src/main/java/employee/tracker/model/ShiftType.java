8c8e4294cc6d7ed0acdf9c2699e62f0e18656938
Bayanda Mnguni <bayandaPM@gmail.com>
2026-04-10 14:30:43 +0200

    added: temporary shiftType enum

---
+++ src/main/java/employee/tracker/model/ShiftType.java
@@ -0,0 +1,22 @@
+package employee.tracker.model;
+
+/**
+ * ShiftType Enum - Defines the type of work shift
+ */
+public enum ShiftType {
+    
+    /**
+     * MORNING - Early shift (typically 6am - 2pm or 9am - 5pm)
+     */
+    MORNING,
+
+    /**
+     * EVENING - Late afternoon/evening shift (typically 2pm - 10pm)
+     */
+    EVENING,
+
+    /**
+     * NIGHT - Overnight shift (typically 10pm - 6am)
+     */
+    NIGHT;
+}
